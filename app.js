6ad4d59efa132b5c70c3877086ae45b043bc8ab4
prodmichael <skimichael21@gmail.com>
2023-09-04 17:18:04 +0300

    connected DB

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
 const courseRoutes = require('./routes/course')
+const keys = require('./config/keys')
 const app = express()
+
+mongoose
+	.connect(keys.mongoURI)
+	.then(() => console.log('MongoDB connected'))
+	.catch((error) => console.log(error))
 
 app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI:
+		'mongodb+srv://prodmichael:D8uudQDrKpwHBI3r@cluster0.ifida77.mongodb.net/?retryWrites=true&w=majority',
+}
