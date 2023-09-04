dadcb66ea587569bffedbbcfaba5fd4247f5abf9
prodmichael <skimichael21@gmail.com>
2023-09-04 16:28:00 +0300

    upd

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
+const courseRoutes = require('./routes/course')
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.use('/api/auth', authRoutes)
+app.use('/api/course', courseRoutes)
 
 module.exports = app
--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ const controller = require('../controllers/auth')
 const router = express.Router()
 
 // .../api/auth/login
-router.get('/login', controller.login)
+router.post('/login', controller.login)
 
-router.get('/register', controller.register)
+router.post('/register', controller.register)
 
 module.exports = router
